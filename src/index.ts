229a26b6452a536bc228dc06bb528a4e3c84203b
hakalaja <hakalaja@msu.edu>
2025-07-31 11:57:54 -0400

    [DEBUG] - update cookie settings

--- src/index.ts
+++ src/index.ts
@@ -42,6 +42,7 @@ app.use(session({
     cookie: {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       maxAge: 1000 * 60 * 60 * 24 // Expires in 24 hours
     }
 }));
